283732b8582a44786e57e3ac0aa47b1f5eb1659f
zhangshuang <zhangshuang@hzdf.com>
2023-01-03 09:19:34 +0800

    使用babel打包ts以适应浏览器版本

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,18 @@ function sum(a: number, b: number): number{
   return a + b
 }
 
+const obj: {
+  name: string,
+  age?: number,
+  [propName: string]: any
+} = {
+  name: 'zs'
+}
+console.log(obj)
+
+obj.age = 24
+console.log(obj)
+
 console.log(sum(123, 456))
 console.log("hello")
 console.log(hi)
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require("path")
 // 引入HTML插件
 const HTMLWebpackPlugin = require("html-webpack-plugin")
-const { CleanWebPackPlugin } = require("clean-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 // webpack中所有的配置信息
 module.exports = {
@@ -18,18 +18,37 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/, // 规则生效文件
-        use: 'ts-loader', // 要使用的loader
+        use: [
+          {
+            loader: "babel-loader", // 指定加载器
+            options: {
+              "presets": [
+                [
+                  "@babel/preset-env", // 指定环境插件
+                  {
+                    targets: {
+                      "chrome": "108"
+                    }, // 要兼容的目标浏览器
+                    "corejs": "3", // 指定corejs版本
+                    "useBuiltIns": "usage" // 使用corejs方法(usage 表示按需加载)
+                  } // 配置信息
+                ]
+              ]
+            } // 设置babel
+          }, // 配置babel
+          'ts-loader'
+        ], // 要使用的loader
         exclude: /node-modules/ // 要排除的文件
       }
     ] // 指定加载规则
   },
   // 配置webpack插件
   plugins: [
+    new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
       // title: "Hello TS"
       template: "./src/index.html"
-    }),
-    new CleanWebPackPlugin()
+    })
   ],
   // 用来设置引用模块
   resolve: {
